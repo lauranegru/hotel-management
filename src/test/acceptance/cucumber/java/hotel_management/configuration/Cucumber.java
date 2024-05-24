7e58767199e135a56961f5d2235428e4006f70ca
lauranegru <125312795+lauranegru@users.noreply.github.com>
2024-05-24 16:12:27 +0200

    Initial commit

---
+++ src/test/acceptance/cucumber/java/hotel_management/configuration/Cucumber.java
@@ -0,0 +1,11 @@
+package hotel_management.configuration;
+
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+//@Suite
+@IncludeEngines("cucumber")
+@SelectPackages("features")
+public class Cucumber {
+}
---
+++ src/test/acceptance/cucumber/java/hotel_management/configuration/Spring.java
@@ -0,0 +1,10 @@
+package hotel_management.configuration;
+
+import hotel_management.Application;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest(classes = Application.class)
+public class Spring {
+}
