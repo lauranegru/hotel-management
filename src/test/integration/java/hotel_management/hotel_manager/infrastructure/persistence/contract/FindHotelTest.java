6bb3857a5e05de860a860bad999d52d5bd3aa1e6
Joan Homs <jhoms.wk@gmail.com>
2024-07-01 19:34:36 +0200

    create-hotel (repository): return the hotel when the hotel exists

---
+++ src/test/integration/java/hotel_management/hotel_manager/infrastructure/persistence/contract/FindHotelTest.java
@@ -0,0 +1,33 @@
+package hotel_management.hotel_manager.infrastructure.persistence.contract;
+
+import hotel_management.hotel_manager.domain.HotelRepository;
+import hotel_management.hotel_manager.infrastructure.persistence.memory.MemoryHotelRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static hotel_management.hotel_manager.domain.HotelGenerator.anyHotel;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FindHotelTest {
+
+    private HotelRepository repository;
+
+    @BeforeEach
+    public void setUp() {
+        repository = new MemoryHotelRepository();
+    }
+
+    @Test
+    void returns_the_hotel_when_the_hotel_exists() {
+        var hotel = anyHotel();
+
+        repository.save(hotel);
+
+        var result = repository
+            .find(hotel.id())
+            .orElseThrow();
+
+        assertThat(result).isEqualTo(hotel);
+    }
+
+}
