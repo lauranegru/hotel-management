7c5d4cc91cd4a709b200dc6584d6eba6798b4595
Joan Homs <jhoms.wk@gmail.com>
2024-07-02 19:43:49 +0200

    create-hotel (repository): deletes the hotel when a single hotel exists

--- src/test/integration/java/hotel_management/hotel_manager/infrastructure/persistence/contract/DeleteHotelTest.java
+++ src/test/integration/java/hotel_management/hotel_manager/infrastructure/persistence/contract/DeleteHotelTest.java
@@ -1,9 +1,12 @@
 package hotel_management.hotel_manager.infrastructure.persistence.contract;
 
+import hotel_management.hotel_manager.domain.HotelGenerator;
 import hotel_management.hotel_manager.domain.HotelRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static hotel_management.hotel_manager.domain.HotelGenerator.anyHotel;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 public abstract class DeleteHotelTest {
@@ -21,6 +24,18 @@ public abstract class DeleteHotelTest {
     void deletes_no_hotels_when_no_hotels_exist() {
         assertThatNoException()
             .isThrownBy(repository::delete);
+    }
+
+    @Test
+    void deletes_the_hotel_when_a_single_hotel_exists() {
+        var hotel = anyHotel();
+
+        repository.save(hotel);
+        repository.delete();
+
+        var result = repository.find(hotel.id());
+
+        assertThat(result).isEmpty();
     }
 
 }
