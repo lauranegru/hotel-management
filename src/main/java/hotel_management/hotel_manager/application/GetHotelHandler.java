a02159a45be01019c22a1c2f9e6b01511c0fddd7
Joan Homs <jhoms.wk@gmail.com>
2024-07-10 19:43:15 +0200

    get-hotel (handler): returns the hotel when the hotel exists

--- src/main/java/hotel_management/hotel_manager/application/GetHotelHandler.java
+++ src/main/java/hotel_management/hotel_manager/application/GetHotelHandler.java
@@ -1,5 +1,6 @@
 package hotel_management.hotel_manager.application;
 
+import hotel_management.hotel_manager.domain.HotelRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -7,8 +8,17 @@ import java.util.Optional;
 @Component
 public class GetHotelHandler {
 
+    private final HotelRepository repository;
+    private final HotelMapper mapper;
+
+    public GetHotelHandler(HotelRepository repository, HotelMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
     public Optional<HotelView> execute(GetHotelQuery query) {
-        return Optional.empty();
+        return repository.find(query.id())
+            .map(mapper::toView);
     }
 
 }
---
+++ src/main/java/hotel_management/hotel_manager/application/HotelMapper.java
@@ -0,0 +1,13 @@
+package hotel_management.hotel_manager.application;
+
+import hotel_management.hotel_manager.domain.Hotel;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HotelMapper {
+
+    public HotelView toView(Hotel hotel) {
+        return null;
+    }
+
+}
