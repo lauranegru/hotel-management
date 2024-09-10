244e5dfd3dc7b8dc3639b052aed3d2b023e231d9
Joan Homs <jhoms.wk@gmail.com>
2024-09-10 19:58:00 +0200

    refactor (delete-hotel-test): remove blockig calls

--- src/test/integration/java/hotel_management/hotel_manager/infrastructure/persistence/contract/DeleteHotelTest.java
+++ src/test/integration/java/hotel_management/hotel_manager/infrastructure/persistence/contract/DeleteHotelTest.java
@@ -4,6 +4,8 @@ import hotel_management.hotel_manager.domain.HotelRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import static hotel_management.hotel_manager.domain.HotelGenerator.anyHotel;
@@ -35,10 +37,10 @@ public abstract class DeleteHotelTest {
     void deletes_the_hotel_when_a_single_hotel_exists() {
         var hotel = anyHotel();
 
-        repository.save(hotel).block();
-        repository.delete().block();
-
-        var result = repository.find(hotel.id());
+        var result = Mono.empty()
+            .then(repository.save(hotel))
+            .then(repository.delete())
+            .then(repository.find(hotel.id()));
 
         StepVerifier.create(result)
             .verifyComplete();
@@ -49,16 +51,20 @@ public abstract class DeleteHotelTest {
         var hotelOne = anyHotel();
         var hotelTwo = anyHotel();
 
-        repository.save(hotelOne).block();
-        repository.save(hotelTwo).block();
+        var result = Flux.concat(
+            Flux.concat(
+                repository.save(hotelOne),
+                repository.save(hotelTwo),
+                repository.delete()
+            ),
+            Flux.merge(
+                repository.find(hotelOne.id()),
+                repository.find(hotelTwo.id())
+            )
+        );
 
-        repository.delete().block();
-
-        var resultOne = repository.find(hotelOne.id());
-        var resultTwo = repository.find(hotelTwo.id());
-
-        StepVerifier.create(resultOne).verifyComplete();
-        StepVerifier.create(resultTwo).verifyComplete();
+        StepVerifier.create(result)
+            .verifyComplete();
     }
 
 }
